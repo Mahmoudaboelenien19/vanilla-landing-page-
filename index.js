48abb676e2776be92de1ac83c1317adb8e569d1a
unknown <103467245+Mahmoudaboelenien19@users.noreply.github.com>
2023-01-24 10:31:23 +0200

    up button animation && functionality

--- index.js
+++ index.js
@@ -1,35 +1,34 @@
-mainImg = document.querySelector(".album .main img");
-top = document.querySelector(".album .top ");
-topImgs = document.querySelectorAll(".album .top img");
-bottom = document.querySelector(".album .bottom");
-bottomImgs = document.querySelectorAll(".album .bottom img");
-upBtn = document.querySelector(".up-btn button")
-nav = document.querySelector("nav")
+const mainImg = document.querySelector( ".album .main img" );
+const topImgs = document.querySelectorAll( ".album .top img" );
+const bottom = document.querySelector( ".album .bottom" );
+const bottomImgs = document.querySelectorAll( ".album .bottom img" );
+const upBtn = document.querySelector( "#up-btn" );
+const nav = document.querySelector( "nav" );
 
 
 
-imgs = [...topImgs, ...bottomImgs];
-console.log(imgs);
-if (localStorage.getItem("srcActive")) {
-  mainImg.src = localStorage.getItem("srcActive");
-  addActive(localStorage.getItem("srcActive"));
+const imgs = [...topImgs, ...bottomImgs];
+
+if ( localStorage.getItem( "srcActive" ) ) {
+  mainImg.src = localStorage.getItem( "srcActive" );
+  addActive( localStorage.getItem( "srcActive" ) );
 } else {
   mainImg.src = "images/creative4.jpg";
-  addImgsToLocal(mainImg.src);
+  addImgsToLocal( mainImg.src );
 }
 
-function f1(x) {
+function f1 ( x ) {
   mainImg.src = x;
-  addImgsToLocal(x);
+  addImgsToLocal( x );
 }
-function mouseover(x) {
+function mouseover ( x ) {
   mainImg.src = x;
-  addImgsToLocal(x);
+  addImgsToLocal( x );
 }
 
-function addImgsToLocal(src) {
-  localStorage.setItem("srcActive", src);
-  addActive(src);
+function addImgsToLocal ( src ) {
+  localStorage.setItem( "srcActive", src );
+  addActive( src );
 }
 
 // function addActive(x) {
@@ -41,28 +40,28 @@ function addImgsToLocal(src) {
 //   }
 // }
 
-function addActive(x) {
-  imgs.forEach((e) =>
-    e.src == x ? (e.style.opacity = 1) : (e.style.opacity = 0.4)
+function addActive ( x ) {
+  imgs.forEach( ( e ) =>
+    e.src == x ? ( e.style.opacity = 1 ) : ( e.style.opacity = 0.4 )
   );
 }
 
-upBtn.style.display = "none"
+
 
 
 onscroll = function () {
 
-  scrollY > 500 ? upBtn.style.display = "block" : upBtn.style.display = "none";
+  scrollY > 500 ? upBtn.classList.remove( "hide" ) : upBtn.classList.add( "hide" );
   nav.onmouseover = () => nav.style.opacity = 1;
   nav.onmouseleave = () => nav.style.opacity = .4;
 
   scrollY > 150 ? nav.style.opacity = .4 : nav.style.opacity = 1;
-}
+};
 upBtn.onclick = function () {
-  scrollTo({
+  scrollTo( {
     top: 0,
     right: 0,
     behaviour: "smooth"
 
-  })
-}
+  } );
+};
